6e91180770470adfabef9db80d3fe22f9a567672
Paco Rilloraza <fpr2017@nyu.edu>
2024-08-16 17:12:57 -0400

    starting cleanup

--- lap3d_dirichlet_routs_test.m
+++
@@ -1,9 +0,0 @@
-S = surfer.sphere(6,1,2,11);
-
-rhs = ones([S.npts 1]);
-dpars = [1.0, 0.0];
-zpars = complex([1.0, 1.0, 0.0]);
-opts_quad = [];
-opts_quad.format='rsc';
-
-Q = lap3d.dirichlet.get_quadrature_correction(S,dpars,eps,S);
